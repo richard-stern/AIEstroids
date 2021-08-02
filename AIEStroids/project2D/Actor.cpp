cf1fdbba0fea2ebd95c2a0e3633afc2886976025
James <james_kemeny@hotmail.com>
2021-08-02 14:28:22 +1000

    Added get/set health to actor class

--- AIEStroids/project2D/Actor.cpp
+++ AIEStroids/project2D/Actor.cpp
@@ -3,12 +3,20 @@
 
 Actor::Actor()
 {
+	m_GlobalTransform.SetPosition({ 0, 0 });
+	SetHealth(MAX_HEALTH);
+}
 
+Actor::Actor(Vector2 _pos)
+{
+	m_GlobalTransform.SetPosition(_pos);
+	SetHealth(MAX_HEALTH);
 }
 
 Actor::~Actor()
 {
-
+	delete m_PhysicsBody;
+	m_PhysicsBody = nullptr;
 }
 
 void Actor::Update()
--- AIEStroids/project2D/Actor.h
+++ AIEStroids/project2D/Actor.h
@@ -1,27 +1,36 @@
 #pragma once
 #include "GameObject.h"
 
+#define MAX_HEALTH 100
+
 // Author: James K
 
 class Actor : public GameObject
 {
 public:
 	Actor();
+	Actor(Vector2 _pos);
 	virtual ~Actor();
 
 	virtual void Update() override;
 
+	int GetHealth() { return m_Health; }
+	void SetHealth(int _health) { m_Health = _health; }
+
 	bool GetWrapAndRespawn() { return m_WrapAndRespawn; }
 	void SetWrapAndRespawn(bool _active) { m_WrapAndRespawn = _active; }
 
-	PhysicsBody* GetPhysicsBody() { return m_PhysicsBody; }
-	void SetPhysicsBody(PhysicsBody* _body) { m_PhysicsBody = _body; }
+	void AddPhysicsBody(PhysicsBody* _body) { m_PhysicsBody = _body; }
+	void RemovePhysicsBody() { m_PhysicsBody = nullptr; }
+
 
 	virtual void OnCollision(CollisionEvent _event);
 
 protected:
 	PhysicsBody* m_PhysicsBody = nullptr;
-
+	
+	int m_Health = 0;
+	
 	bool m_WrapAndRespawn = false;
 };
 
