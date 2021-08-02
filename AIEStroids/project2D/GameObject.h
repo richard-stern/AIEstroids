4c544d8bbaee3afc56ac3ef045a7d384c3e05adb
James <james_kemeny@hotmail.com>
2021-08-02 12:18:35 +1000

    Added Actor class, extra GO functions

--- AIEStroids/project2D/GameObject.h
+++ AIEStroids/project2D/GameObject.h
@@ -3,6 +3,8 @@
 #include "Texture.h"
 #include "Matrix3.h"
 #include "Vector2.h"
+#include "DynamicArray.h"
+
 
 class CollisionEvent;
 
@@ -10,23 +12,47 @@ class GameObject
 {
 public:
 	GameObject();
-	GameObject(float _posX, float _posY);
+	GameObject(Vector2 _pos);
 	virtual ~GameObject();
 
 	virtual void Update(float _deltaTime);
 	virtual void Draw(aie::Renderer2D* _renderer2D);
 
-	Vector2 GetPosition() { return m_Position; }
-	void SetPosition(Vector2 _pos) { m_Position = _pos; }
+	GameObject* GetParent() { return m_Parent; }
+	void SetParent(GameObject* _parent) { m_Parent = _parent; }
 
+	DynamicArray<GameObject*> GetChildren() { return m_Children; }
+	void SetChild(GameObject* _child) { m_Children.Add(_child); }
+
+	Vector2 GetLocalPosition() { return m_LocalTransform.GetPosition(); }
+	void SetLocalPosition(Vector2 _pos) { m_LocalTransform.SetPosition(_pos); }
+
+	Vector2 GetVelocity() { return m_Velocity; }
+	void SetVelocity(Vector2 _velocity) { m_Velocity = _velocity; }
+
+	float GetDrag() { return m_Drag; }
+	void SetDrag(float _drag) { m_Drag = _drag; }
+
+	bool GetActive() { return m_IsActive; }
+	void SetActive(bool _active) { m_IsActive = _active; }
+
+	aie::Texture* GetTexture() { return m_Texture; }
+	void SetTexture(aie::Texture* _texture) { m_Texture = _texture; }
 
 	//virtual void OnCollision(CollisionEvent _event);
 
-
 private:
+	GameObject* m_Parent = nullptr;
+	DynamicArray<GameObject*> m_Children;
+
 	Matrix3 m_GlobalTransform;
-	Vector2 m_Position;
+	Matrix3 m_LocalTransform;
+
+	Vector2 m_Velocity = { 0, 0 };
+	float m_Drag = 0;
+
 	aie::Texture* m_Texture;
-	bool m_IsActive;
+	bool m_IsActive = false;
+	bool m_WrapAndRespawn = false;
 };
 
