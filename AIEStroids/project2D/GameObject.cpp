403274073557e90b6487c18ec7ac6f99b6ff35ff
James <james_kemeny@hotmail.com>
2021-08-03 14:56:25 +1000

    Commenting

--- AIEStroids/project2D/GameObject.cpp
+++ AIEStroids/project2D/GameObject.cpp
@@ -27,6 +27,7 @@ GameObject::~GameObject()
 
 void GameObject::Update(float _deltaTime)
 {
+	//loop through all child objects and update
 	for (int i = 0; i < m_Children.Count(); i++)
 		m_Children[i]->Update(_deltaTime);
 }
@@ -51,13 +52,13 @@ void GameObject::UpdateTransforms()
 
 void GameObject::Draw(aie::Renderer2D* _renderer2D)
 {
+	//loop through all child objects and draw
 	for (int i = 0; i < m_Children.Count(); i++)
 		m_Children[i]->Draw(_renderer2D);
 
 	if (m_IsActive)
 	{
 		_renderer2D->DrawSpriteTransformed3x3(m_Texture, m_GlobalTransform.m);
-
 	}
 }
 
