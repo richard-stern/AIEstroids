568eeecdfe14ffb71b9f245264da95bc9ee46bf9
Matin <matincode@gmail.com>
2021-08-03 11:52:30 +1000

    Latest work on GameState.cpp

--- AIEStroids/project2D/GameState.cpp
+++ AIEStroids/project2D/GameState.cpp
@@ -1,25 +1,37 @@
 // Author: Matin Kamali
 #include "GameState.h"
+#include "GUI.h"
+#include "Level.h"
 
-///
+/// init level and GUI to nullptr
 GameState::GameState() {
-	// @incomplete
+	level = nullptr;
+	gui   = nullptr;
 };
-///
+/// Destory the level and GUI
 GameState::~GameState() {
-	// @incomplete
+	delete level;
+	// delete gui; // @incomplete gui is a singleton, we shouldn't be deleting it
 };
-///
+/// Load and initialise the level and GUI
 void GameState::Enter() {
-	// @incomplete
+	level = new Level();
+	gui   = GUI::GetInstance();
 };
 ///
 void GameState::Update(float deltaTime, StateMachine* state) {
-	// @incomplete
+	// @incomplete check if the player's health is zero
+	// if so, change state to game over state
+	level->Update(deltaTime);
 };
 ///
 void GameState::Draw(aie::Renderer2D* renderer) {
-	// @incomplete
+	level->Draw(renderer);
+	// @incomplete ask GUI.h to not take in input, it's a singleton and they should get it themselves
+	aie::Input* input = aie::Input::GetInstance();
+	// @incomplete ask GUI.h to not take in font, they should be handling the font and not the GameState
+	auto font = TextureManager::Get()->LoadFont(""); // @incomplete pass a file path
+	gui->Draw(renderer, font, input);
 };
 ///
 void GameState::Exit() {
