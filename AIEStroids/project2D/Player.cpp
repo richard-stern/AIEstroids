3c8587c0b67bd1cc1ab265c886107d3aff4309e0
Rahul <rahul.jhugroo99@gmail.com>
2021-08-03 16:13:42 +1000

    Added turret toplayer

--- AIEStroids/project2D/Player.cpp
+++ AIEStroids/project2D/Player.cpp
@@ -24,6 +24,15 @@ Player::Player(Vector2 startPos) : Actor::Actor(startPos)
 	Collider* collider = new Collider(shape, (unsigned short)CollisionLayer::PLAYER, layermask);
 	//Create the physics body using the generated collider
 	m_PhysicsBody = (new PhysicsBody(this, BodyType::DYNAMIC, collider));
+
+	//------------------CREATE TURRET----------------------//
+	turret = new Turret();
+	//turret->SetParent(this);
+	AddChild(turret);
+	//turret->SetPos(1000.0f, 0.0f);
+	turret->SetLocalPosition(Vector2(100.0f, 0.0f));
+
+
 	gui = GUI::GetInstance();
 }
 
@@ -33,6 +42,7 @@ Player::~Player()
 
 void Player::Update(float deltaTime)
 {
+	Actor::Update(deltaTime);
 	if (playerAlive)
 	{
 
@@ -69,7 +79,7 @@ void Player::Update(float deltaTime)
 			thrustAmount *= PLAYER_COUNTERFORCE_MULT;
 
 		//Scale up torque amount if rotating away from current rotational velocity
-		if (currentAngularVelocity * inputVector.x < 0)
+		if (currentAngularVelocity * -inputVector.x < 0)
 			torqueAmount *= PLAYER_COUNTERTORQUE_MULT;
 
 		//--------------
@@ -83,7 +93,7 @@ void Player::Update(float deltaTime)
 			m_PhysicsBody->SetVelocity(currentVelocity.GetNormalised() * PLAYER_MAXSPEED);
 
 		//-------------------------R O T A T I O N----------------------------------------------------
-		m_PhysicsBody->AddAngularVelocity(-inputVector.x * torqueAmount * DEG2RAD * deltaTime);
+		m_PhysicsBody->AddAngularVelocity(-inputVector.x * torqueAmount * deltaTime);
 		//Limit angular velocity
 		currentAngularVelocity = m_PhysicsBody->GetAngularVelocity();
 		float absoluteValue = std::abs(currentAngularVelocity);
@@ -94,6 +104,8 @@ void Player::Update(float deltaTime)
 		//Check tha health
 		if (m_CurrentHealth <= 0)
 			KillPlayer();
+
+		turret->Update(deltaTime);
 
 	}
 	else
