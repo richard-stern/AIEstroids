2ae0f5d19c0201c3c3ec6b2d7258a8d3b9f93964
Matin <matincode@gmail.com>
2021-08-03 16:14:03 +1000

    StateMachine: start from menu

--- AIEStroids/project2D/StateMachine.cpp
+++ AIEStroids/project2D/StateMachine.cpp
@@ -11,7 +11,7 @@ StateMachine::StateMachine(aie::Renderer2D* _renderer) {
 	game_state      = new GameState(_renderer);
 	game_over_state = new GameOverState();
 	current_state = nullptr;
-	ChangeState(ESTATE_GAME); // @incomplete change this to menu
+	ChangeState(ESTATE_MENU);
 	renderer = _renderer;
 };
 ///
